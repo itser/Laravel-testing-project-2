0002bdb9cf691f1d6b56c376f1ff92fdfa6b4962
itser <itser1106@gmail.com>
2019-10-24 23:57:39 +0300

    no message

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -3,7 +3,9 @@
     <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>Users</span></a>
 </li>
 @endhasrole
+@can('role-list')
 <li class="{{ Request::is('roles*') ? 'active' : '' }}">
     <a href="{!! route('roles.index') !!}"><i class="fa fa-edit"></i><span>Roles</span></a>
 </li>
+@endcan
 
