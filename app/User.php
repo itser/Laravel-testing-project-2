2429ae819c13c5ec676f93be586448de0c7da3e7
itser <itser1106@gmail.com>
2019-10-24 20:38:31 +0300

    no message

---
+++ app/User.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App;
+
+use Eloquent as Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
+
+/**
+ * Class User
+ * @package App\Models
+ * @version October 24, 2019, 5:11 pm UTC
+ *
+ * @property string name
+ * @property string email
+ * @property string|\Carbon\Carbon email_verified_at
+ * @property string password
+ * @property string remember_token
+ */
+class User extends Authenticatable
+{
+    use Notifiable;
+    use HasRoles;
+
+    public $table = 'users';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+    protected $dates = ['deleted_at'];
+
+
+    public $fillable = [
+        'name',
+        'email',
+        'email_verified_at',
+        'password',
+        'remember_token'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'string',
+        'email' => 'string',
+        'email_verified_at' => 'datetime',
+        'password' => 'string',
+        'remember_token' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'required',
+        'email' => 'required',
+        'password' => 'required'
+    ];
+
+    
+}
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,8 @@
+<li class="{{ Request::is('roles*') ? 'active' : '' }}">
+    <a href="{!! route('roles.index') !!}"><i class="fa fa-edit"></i><span>Roles</span></a>
+</li>
+
+<li class="{{ Request::is('users*') ? 'active' : '' }}">
+    <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>Users</span></a>
+</li>
+
